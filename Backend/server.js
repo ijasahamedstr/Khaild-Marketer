badf4e1a748b22b2150c6edb45a9650fb14ade7e
ijasahamedstr <ijasahamed.str@gmail.com>
2026-01-13 19:48:47 +0300

    commit

--- Backend/server.js
+++ Backend/server.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",
+      "https://www.waseetaqary.com",
       "http://localhost:3002",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
