94f3d6df2df97ddb7379b8eee86291e965b6c8af
ijasahamedstr <ijasahamed.str@gmail.com>
2026-02-03 17:57:17 +0300

    commit

--- Backend/server.js
+++ Backend/server.js
@@ -20,8 +20,8 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-app.use(express.json({ limit: '5mb' }));
-app.use(express.urlencoded({ limit: '5mb', extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(express.json());
 // Serve the uploads folder statically so you can view images via URL
